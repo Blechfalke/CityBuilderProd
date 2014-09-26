28308e2942d5fbc4b93b916d756d110245d6e0d4
Alena <Alena@10.91.130.180>
2014-09-26 09:49:33 +0200

    new pages, jquery-ui

--- view/adminMenu.php
+++ view/adminMenu.php
@@ -1,16 +1,11 @@
 ﻿<?php 
-// echo "<div id='mainButtonDiv'>";
-// echo "<h2 id='gameName'>City builders</h2>";
-// echo "<input type='button' value='Launch the game' name='CityManagement' class='mainButtons link'/>";
-// echo "<input type='button' value='Rules' name='Rules' class='mainButtons link'/>";
-// echo "<input type='button' value='Game modes' name='GameModes' class='mainButtons link'/>";
-// echo "<input type='button' value='Exit game' name='ExitGame' class='mainButtons link'/>";
-// echo "</div>"
-echo "<div id='mainButtonDiv'>
-<h2 id='gameName'>City builders</h2>
-<input type='button' value='Launch the game' name='CityManagement' class='mainButtons link'/>
-<input type='button' value='Rules' name='Rules' class='mainButtons link'/>
-<input type='button' value='Game modes' name='GameModes' class='mainButtons link'/>
-<input type='button' value='Exit game' name='ExitGame' class='mainButtons link'/>
-</div>";
+
+echo "
+        <div id='mainButtonDiv'>
+        <h2 id='gameName'>City builders</h2>
+        <input type='button' value='Launch the game' name='CityManagement' class='mainButtons link'/>
+        <input type='button' value='Rules' name='Rules' class='mainButtons link'/>
+        <input type='button' value='Game modes' name='GameModes' class='mainButtons link'/>
+        <input type='button' value='Exit game' name='ExitGame' class='mainButtons link'/>
+        </div>";
 ?>
