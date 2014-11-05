bdfa1fd53972520c8f4dace073f2c77fd961aac7
Alena <Alena@10.39.1.95>
2014-11-05 14:19:22 +0100

    login page changes

--- view/login.php
+++ view/login.php
@@ -3,7 +3,7 @@ require_once '../config.php';
 header('Content-type: text/html; charset=UTF-8');
 $msg = isset($_SESSION['msg']) ? $_SESSION['msg'] : null;
 ?>
-<div id="header">Bâtisseur de cité</div>
+<div id="header" style="padding: 100px 0 50px 0;">Bâtisseur de cité</div>
     <div id='mainButtonDiv'>
     <input type='text' name='Username' id='username' class='loginForm' placeholder='Username' value='maurice'/>
     <input type='password' name='Password' id='password' class='loginForm' placeholder='Password' value='test123'/>
@@ -12,8 +12,9 @@ $msg = isset($_SESSION['msg']) ? $_SESSION['msg'] : null;
     	<option value='en_GB.UTF-8'>English</option>
     	<option value='fr_CH.UTF-8'>Français</option>
     </select>
-    <a href="javascript:void(0)" class="register">Register</a>
+    
 	<input type='button' value='Login' name='Login' class='mainButtons login'/>
+	<a href="javascript:void(0)" class="register">Register</a>
     <?php if ($msg != null)
     	  	echo "<p id='errorMSG'>$msg</p>";?>    
 </div>
