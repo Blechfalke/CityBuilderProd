fa4eefb2ae446f9ed76266b70d35457c3a1cda22
EricReichenbach <eric.reichenbach@live.fr>
2014-11-05 15:02:29 +0100

    Modified the login to get rid of the login informations
    BTW the admin account is:
    adminpyramid
    admin_pyramid_42

--- view/login.php
+++ view/login.php
@@ -12,8 +12,8 @@ $msg = isset($_SESSION['msg']) ? $_SESSION['msg'] : null;
 ?>
 <div id="header" style="padding: 100px 0 50px 0;">Bâtisseur de cité</div>
     <div id='mainButtonDiv'>
-    <input type='text' name='Username' id='username' class='loginForm' placeholder='Username' value='maurice'/>
-    <input type='password' name='Password' id='password' class='loginForm' placeholder='Password' value='test123'/>
+    <input type='text' name='Username' id='username' class='loginForm' placeholder='Username' value=''/>
+    <input type='password' name='Password' id='password' class='loginForm' placeholder='Password' value=''/>
     <select class='locale' name='locale' id='langdropdown'>
 		<!--<option value='de_DE.UTF-8'>Deutsch</option>-->
     	<option value='en_GB.UTF-8'>English</option>
